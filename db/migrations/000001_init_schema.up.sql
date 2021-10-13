286e091a860705d41c0a4e65086ce05c3f6b819c
Ashish Kumar <ashishkr23438@gmail.com>
2021-10-13 19:55:35 +0530

    feat(db): Add db init schema: tables
    Signed-off-by: Ashish Kumar <ashishkr23438@gmail.com>

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -79,3 +79,200 @@ AS $BODY$
       ('x' || translate(gen_random_uuid()::TEXT, '-', ''))::BIT(64)::BIGINT
     );
 $BODY$ LANGUAGE SQL VOLATILE STRICT;
+
+
+
+-------------------------------------------------------------------------------
+-- TABLES
+-------------------------------------------------------------------------------
+
+
+-- LOCATIONS TABLE start
+CREATE TABLE locations (
+  id          BIGINT PRIMARY KEY,
+  location    geometry(Point, 4326) GENERATED ALWAYS AS (id_to_geom(id)) STORED,
+  latitude    FLOAT GENERATED ALWAYS AS (ST_Y(id_to_geom(id))) STORED,
+  longitude   FLOAT GENERATED ALWAYS AS (ST_X(id_to_geom(id))) STORED,
+
+  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
+-- LOCATIONS TABLE end
+
+
+-- PROJECTS TABLE start
+CREATE TABLE projects (
+  id          BIGINT    DEFAULT random_bigint() PRIMARY KEY,
+  name        VARCHAR   NOT NULL,
+
+  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  deleted     BOOLEAN   NOT NULL DEFAULT FALSE,
+
+  CHECK(id >= 0)
+);
+-- PROJECTS TABLE end
+
+
+-- PROJECT LOCATIONS TABLE start
+-- Aggregates all the locations in a project, eases inserting rows in the matrix
+CREATE TABLE project_locations (
+  project_id  BIGINT    NOT NULL REFERENCES projects(id),
+  location_id BIGINT    NOT NULL REFERENCES locations(id),
+
+  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
+-- PROJECT LOCATIONS TABLE end
+
+
+-- JOBS TABLE start
+CREATE TABLE jobs (
+  id              BIGINT    DEFAULT random_bigint() PRIMARY KEY,
+  location_index  BIGINT    NOT NULL REFERENCES locations(id),
+  service         INTERVAL  NOT NULL DEFAULT '00:00:00'::INTERVAL,
+  delivery        BIGINT[]  NOT NULL DEFAULT ARRAY[]::BIGINT[],
+  pickup          BIGINT[]  NOT NULL DEFAULT ARRAY[]::BIGINT[],
+  skills          INTEGER[] NOT NULL DEFAULT ARRAY[]::INTEGER[],
+  priority        INTEGER   NOT NULL DEFAULT 0,
+
+  created_at      TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at      TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  deleted         BOOLEAN   NOT NULL DEFAULT FALSE,
+
+  CHECK(id >= 0),
+  CHECK(service >= '00:00:00'::INTERVAL),
+  CHECK(0 <= ALL(delivery)),
+  CHECK(0 <= ALL(pickup)),
+  CHECK(0 <= ALL(skills)),
+  CHECK(priority >= 0 AND priority <= 100)
+);
+-- JOBS TABLE end
+
+
+-- JOBS TIME WINDOWS TABLE start
+CREATE TABLE jobs_time_windows (
+  id          BIGINT    NOT NULL REFERENCES jobs(id),
+  tw_open     TIMESTAMP NOT NULL,
+  tw_close    TIMESTAMP NOT NULL,
+
+  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  deleted     BOOLEAN   NOT NULL DEFAULT FALSE,
+
+  CHECK(tw_open <= tw_close)
+);
+-- JOBS TIME WINDOWS TABLE end
+
+
+-- SHIPMENTS TABLE start
+CREATE TABLE shipments (
+  id                BIGINT    DEFAULT random_bigint() PRIMARY KEY,
+  p_location_index  BIGINT    NOT NULL REFERENCES locations(id),
+  p_service         INTERVAL  NOT NULL DEFAULT '00:00:00'::INTERVAL,
+  d_location_index  BIGINT    NOT NULL REFERENCES locations(id),
+  d_service         INTERVAL  NOT NULL DEFAULT '00:00:00'::INTERVAL,
+  amount            BIGINT[]  NOT NULL DEFAULT ARRAY[]::BIGINT[],
+  skills            INTEGER[] NOT NULL DEFAULT ARRAY[]::INTEGER[],
+  priority          INTEGER   NOT NULL DEFAULT 0,
+
+  created_at        TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at        TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  deleted           BOOLEAN   NOT NULL DEFAULT FALSE,
+
+  CHECK(id >= 0),
+  CHECK(p_service >= '00:00:00'::INTERVAL),
+  CHECK(d_service >= '00:00:00'::INTERVAL),
+  CHECK(0 <= ALL(amount)),
+  CHECK(0 <= ALL(skills)),
+  CHECK(priority >= 0 AND priority <= 100)
+);
+-- SHIPMENTS TABLE end
+
+
+-- SHIPMENTS TIME WINDOWS TABLE start
+CREATE TABLE shipments_time_windows (
+  id          BIGINT    NOT NULL REFERENCES shipments(id),
+  kind        CHAR(1)   NOT NULL,
+  tw_open     TIMESTAMP NOT NULL,
+  tw_close    TIMESTAMP NOT NULL,
+
+  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  deleted     BOOLEAN   NOT NULL DEFAULT FALSE,
+
+  CHECK(kind = 'p' OR kind = 'd'),
+  CHECK(tw_open <= tw_close)
+);
+-- SHIPMENTS TIME WINDOWS TABLE end
+
+
+-- VEHICLES TABLE start
+CREATE TABLE vehicles (
+  id            BIGINT    DEFAULT random_bigint() PRIMARY KEY,
+  start_index   BIGINT    NOT NULL REFERENCES locations(id),
+  end_index     BIGINT    NOT NULL REFERENCES locations(id),
+  capacity      BIGINT[]  NOT NULL DEFAULT ARRAY[]::BIGINT[],
+  skills        INTEGER[] NOT NULL DEFAULT ARRAY[]::INTEGER[],
+  tw_open       TIMESTAMP NOT NULL DEFAULT (to_timestamp(0) at time zone 'UTC'),
+  tw_close      TIMESTAMP NOT NULL DEFAULT (to_timestamp(2147483647) at time zone 'UTC'),
+  speed_factor  FLOAT     NOT NULL DEFAULT 1.0,
+
+  created_at    TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at    TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  deleted       BOOLEAN   NOT NULL DEFAULT FALSE,
+
+  CHECK(id >= 0),
+  CHECK(0 <= ALL(capacity)),
+  CHECK(0 <= ALL(skills)),
+  CHECK(tw_open <= tw_close),
+  CHECK(speed_factor > 0.0)
+);
+-- VEHICLES TABLE end
+
+
+-- BREAKS TABLE start
+CREATE TABLE breaks (
+  id          BIGINT    DEFAULT random_bigint() PRIMARY KEY,
+  vehicle_id  BIGINT    NOT NULL REFERENCES vehicles(id),
+  service     INTERVAL  NOT NULL DEFAULT '00:00:00'::INTERVAL,
+
+  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  deleted     BOOLEAN   NOT NULL DEFAULT FALSE,
+
+  CHECK(id >= 0),
+  CHECK(service >= '00:00:00'::INTERVAL)
+);
+-- BREAKS TABLE end
+
+
+-- BREAKS TIME WINDOWS TABLE start
+CREATE TABLE breaks_time_windows (
+  id          BIGINT    NOT NULL REFERENCES breaks(id),
+  tw_open     TIMESTAMP NOT NULL,
+  tw_close    TIMESTAMP NOT NULL,
+
+  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  deleted     BOOLEAN   NOT NULL DEFAULT FALSE,
+
+  CHECK(tw_open <= tw_close)
+);
+-- BREAKS TIME WINDOWS TABLE end
+
+
+-- MATRIX TABLE start
+CREATE TABLE matrix (
+  start_vid   BIGINT    NOT NULL REFERENCES locations(id),
+  end_vid     BIGINT    NOT NULL REFERENCES locations(id),
+  agg_cost    INTEGER   NOT NULL,
+
+  created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+  PRIMARY KEY (start_vid, end_vid),
+
+  CHECK(agg_cost >= 0)
+);
+-- MATRIX TABLE end
