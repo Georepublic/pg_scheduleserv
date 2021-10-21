1691c2e11a2fe8124ca14681ab2cf3e099bffb43
Ashish Kumar <ashishkr23438@gmail.com>
2021-10-22 00:07:43 +0530

    feat(config): Add config and env file
    Signed-off-by: Ashish Kumar <ashishkr23438@gmail.com>

---
+++ internal/config/config.go
@@ -0,0 +1,52 @@
+/*GRP-GNU-AGPL******************************************************************
+
+File: config.go
+
+Copyright (C) 2021  Team Georepublic <info@georepublic.de>
+
+Developer(s):
+Copyright (C) 2021  Ashish Kumar <ashishkr23438@gmail.com>
+
+-----
+
+This file is part of pg_scheduleserv.
+
+pg_scheduleserv is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pg_scheduleserv is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with pg_scheduleserv.  If not, see <https://www.gnu.org/licenses/>.
+
+******************************************************************GRP-GNU-AGPL*/
+
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DatabaseURL       string `mapstructure:"DATABASE_URL"`
+	ServerBindAddress string `mapstructure:"SERVER_BIND_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
