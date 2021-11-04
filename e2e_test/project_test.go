1351f6d2fc882ac3b10955a66118457b30d8f745
Ashish Kumar <ashishkr23438@gmail.com>
2021-11-04 23:34:39 +0530

    refactor(e2e_test): Change package name, add license
    Signed-off-by: Ashish Kumar <ashishkr23438@gmail.com>

--- e2e_test/project_test.go
+++ e2e_test/project_test.go
@@ -1,4 +1,32 @@
-package test
+/*GRP-GNU-AGPL******************************************************************
+
+File: project_test.go
+
+Copyright (C) 2021  Team Georepublic <info@georepublic.de>
+
+Developer(s):
+Copyright (C) 2021  Ashish Kumar <ashishkr23438@gmail.com>
+
+-----
+
+This file is part of pg_scheduleserv.
+
+pg_scheduleserv is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pg_scheduleserv is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with pg_scheduleserv.  If not, see <https://www.gnu.org/licenses/>.
+
+******************************************************************GRP-GNU-AGPL*/
+
+package e2etest
 
 import (
 	"bytes"
