0ebb583af7f089d326a1e030dff96649b208a729
Ashish Kumar <ashishkr23438@gmail.com>
2022-02-13 17:06:59 +0530

    feat(demo): Add project parameters in frontend
    Signed-off-by: Ashish Kumar <ashishkr23438@gmail.com>

--- demo/js/api/ProjectAPI.js
+++ demo/js/api/ProjectAPI.js
@@ -1,3 +1,4 @@
+import Parser from "../utils/Parser.js";
 import BaseAPI from "./BaseAPI.js";
 
 export default class ProjectAPI {
@@ -5,11 +6,28 @@ export default class ProjectAPI {
     this.baseAPI = new BaseAPI();
   }
 
+  parseProject(data) {
+    return {
+      id: data.id,
+      name: data.name,
+      duration_calc: data.duration_calc,
+      max_shift: Parser.parseDuration(data.max_shift),
+      exploration_level: parseInt(data.exploration_level),
+      timeout: Parser.parseDuration(data.timeout),
+    };
+  }
+
   listProjects() {
     return this.baseAPI.get(`/projects`);
   }
 
   saveProject(data) {
+    try {
+      data = this.parseProject(data);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+
     if (data["id"]) {
       return this.baseAPI.patch(`/projects/${data["id"]}`, data);
     } else {
